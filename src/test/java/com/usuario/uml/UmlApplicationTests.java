345833b90e836624204c2aaf734746c199479189
Yohannah Véras <yohannahlv@gmail.com>
2025-04-23 10:02:23 -0300

    projeto criado

---
+++ src/test/java/com/usuario/uml/UmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuario.uml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
