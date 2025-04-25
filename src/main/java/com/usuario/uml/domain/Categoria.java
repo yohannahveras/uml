4a6ba8f9335bf1cb86284564922b9d80f2f9d2b9
Yohannah Véras <yohannahlv@gmail.com>
2025-04-25 18:55:48 -0300

    banco de dados postgres e criacao automatica da base de dados

--- src/main/java/com/usuario/uml/domain/Categoria.java
+++ src/main/java/com/usuario/uml/domain/Categoria.java
@@ -1,9 +1,19 @@
 package com.usuario.uml.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
